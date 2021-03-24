0ad483d89baea6777618a3ee7fbeceb54b07be2d
ZeCosta <josepedroc.costa@gmail.com>
2021-03-24 23:15:58 +0000

    c a funcionar, falta por tudo em json

---
+++ c.py
@@ -0,0 +1,34 @@
+import re
+f = open("exemplo-utf8.bib", "r", encoding='utf-8')
+
+json = open("c.json","w")
+
+categoria=[]
+autores = []
+
+
+
+#HOW TO SPLI 
+l = f.read()
+categoria = re.split('@',l)
+first=True
+
+json.write("{\n\t\"referencias\":[")
+for i in categoria:
+	print(i)
+	tipo = re.match(r'(\w+)\{([A-Z1-9]+)',i)
+	
+	if tipo:
+		g=re.split('{',tipo.group())
+		print("tipo: ",g[0])
+		print("codigo: ",g[1])
+
+
+
+	info = re.findall(r'\b(\w+)[ ]*=[ ]*\{?\"?([\w ]*)',i)
+	for (c,v) in info:
+		print(c,": ",v)
+	
+
+	print()
+	print()
